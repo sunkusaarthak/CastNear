7dcfa9ba744ab2453652d2da56679fcd4bb32fae
Sunku Saarthak <sunkusaarthak@gmail.com>
2023-02-13 17:08:44 +0530

    Added BroadCastMessage.kt

---
+++ app/src/main/java/com/example/castnear/BroadCastMessage.kt
@@ -0,0 +1,11 @@
+package com.example.castnear
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class BroadCastMessage : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_broad_cast_message)
+    }
+}
